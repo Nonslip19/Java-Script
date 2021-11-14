afdafb9469b8bf272227c44bf391bea9365698bb
Mehmet <mkaymaz93@gmail.com>
2021-11-14 19:24:45 +0300

    iteration

---
+++ code-challenges/student Registration/app.js
@@ -0,0 +1,11 @@
+var students =  ['C1234 - John Doe, London, Full-Stack',
+'C2345 - Jane Doe, London, Data-Science',
+'C2346 - Mary Ann, Paris, AWS-Devops',
+'C2347 - Adam Smith, Texas, AWS-Devops',
+'C2444 - Michael Great, Arizona, Full-Stack',
+'C2555 - William Cash, Manchester, Data-Science',
+'C2455 - Patrick Jane, Madrid, Full-Stack']
+
+var student1 = students[0].split(" ");
+
+console.log(student1);
