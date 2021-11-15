5dc7acb7308b4f7502db3007ca247c218464eddf
Mehmet <mkaymaz93@gmail.com>
2021-11-15 11:59:38 +0300

    iterator

--- deneme/app.js
+++ deneme/app.js
@@ -1,3 +1,48 @@
-var colors = ["Red", "Green", "Yellow", "Green" , "Blue", "Green", "Pink", "Green", "Black", "Green"];
-var x = colors.lastIndexOf("Green", 4);
-console.log(x);
+var numbers = [1, 2, 3, 4, 5, 6];
+var kare = numbers.map((number) => number * number);
+console.log(kare);
+
+var filtre = numbers.filter((number) => !(number % 2));
+console.log(filtre);
+
+var topla = numbers.reduce((x, y) => x - y);
+console.log(topla);
+
+function withVar() {
+  const b = () => a;
+  var a = 24;
+  return b;
+}
+
+function withLet() {
+  const b = () => a;
+  let a = 24;
+  return b;
+}
+
+function changingValue() {
+  let a = 24;
+  const b = () => a;
+  a = 42;
+  return b;
+}
+
+console.log(withVar()()); // ??
+console.log(withLet()()); // ??
+console.log(changingValue()()); // ??
+
+var obj = { w: 1, e: 2, r: 3 };
+for (let a in obj) {
+  console.log(a); //w e r
+}
+
+var numbers = [1, 2, 3, 4, 2, 5, 6];
+for (b of numbers) {
+  console.log(b);
+}
+
+const meyve = ["Elma", "Muz", "Kiraz"];
+
+for (let i in meyve) {
+  console.log(i, ":", meyve[i]);
+}
